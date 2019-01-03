38e70d45748224110ea11304a6d19f9c6a9d1127
rgraf <roman.graf@ait.ac.at>
2019-01-03 17:47:33 +0100

    #17 EA-1446 URIs profiles updated according to the corrected specification

--- set-definitions/src/main/java/eu/europeana/set/definitions/model/vocabulary/ProfileKeyword.java
+++ set-definitions/src/main/java/eu/europeana/set/definitions/model/vocabulary/ProfileKeyword.java
@@ -15,7 +15,7 @@ public interface ProfileKeyword {
 	public String getPreferHeaderValue();
 
 	public static final String VALUE_LD_CONTAINEDIRIS = "http://www.w3.org/ns/oa#PreferContainedIRIs";
-	public static final String VALUE_LD_MINIMAL = "http://www.w3.org/ns/oa#PreferMinimalContainer";
+	public static final String VALUE_LD_MINIMAL = "http://www.w3.org/ns/ldp#PreferMinimalContainer";
 
 	public static final String VALUE_PREFER_CONTAINEDIRIS = "return=representation;include=\"" + VALUE_LD_CONTAINEDIRIS + "\"";
 	public static final String VALUE_PREFER_MINIMAL = "return=representation;include=\"" +VALUE_LD_MINIMAL+ "\"";
