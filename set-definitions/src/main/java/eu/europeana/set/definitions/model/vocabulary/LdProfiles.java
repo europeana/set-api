b2bd6924e5fbb9ab80b5b5cd95f987d66525a117
GordeaS <GordeaS@N3DSS1923.D03.arc.local>
2020-09-18 08:59:32 +0200

    #96 addressing major issues reported by sonar cube

--- set-definitions/src/main/java/eu/europeana/set/definitions/model/vocabulary/LdProfiles.java
+++ set-definitions/src/main/java/eu/europeana/set/definitions/model/vocabulary/LdProfiles.java
@@ -1,5 +1,12 @@
 package eu.europeana.set.definitions.model.vocabulary;
 
+import static eu.europeana.set.definitions.model.vocabulary.ProfileConstants.VALUE_LD_CONTAINEDIRIS;
+import static eu.europeana.set.definitions.model.vocabulary.ProfileConstants.VALUE_LD_ITEM_DESCRIPTIONS;
+import static eu.europeana.set.definitions.model.vocabulary.ProfileConstants.VALUE_LD_MINIMAL;
+import static eu.europeana.set.definitions.model.vocabulary.ProfileConstants.VALUE_PREFER_CONTAINEDIRIS;
+import static eu.europeana.set.definitions.model.vocabulary.ProfileConstants.VALUE_PREFER_ITEM_DESCRIPTIONS;
+import static eu.europeana.set.definitions.model.vocabulary.ProfileConstants.VALUE_PREFER_MINIMAL;
+
 import eu.europeana.set.definitions.exception.UserSetProfileValidationException;
 
 /**
@@ -10,62 +17,63 @@ import eu.europeana.set.definitions.exception.UserSetProfileValidationException;
  */
 public enum LdProfiles implements ProfileKeyword {
 
-	MINIMAL(VALUE_LD_MINIMAL, VALUE_PREFER_MINIMAL), STANDARD(VALUE_LD_CONTAINEDIRIS, VALUE_PREFER_CONTAINEDIRIS), ITEMDESCRIPTIONS(VALUE_LD_ITEM_DESCRIPTIONS, VALUE_PREFER_ITEM_DESCRIPTIONS);
-	
-	private String headerValue;
-	private String preferHeaderValue;
-	
+    MINIMAL(VALUE_LD_MINIMAL, VALUE_PREFER_MINIMAL), STANDARD(VALUE_LD_CONTAINEDIRIS, VALUE_PREFER_CONTAINEDIRIS),
+    ITEMDESCRIPTIONS(VALUE_LD_ITEM_DESCRIPTIONS, VALUE_PREFER_ITEM_DESCRIPTIONS);
 
-	LdProfiles(String headerValue, String preferHeaderValue){
-		this.headerValue = headerValue; 
-		this.preferHeaderValue = preferHeaderValue;
-	}
-	
-	/**
-	 * Identifying requested profile by Linked Data value.
-	 * For user friendliness the the comparison is case insensitive  
-	 * @param headerValue
-	 * @return
-	 * @throws UserSetProfileValidationException 
-	 */
-	public static LdProfiles getByHeaderValue(String headerValue) throws UserSetProfileValidationException{
-		
-		for(LdProfiles ldType : LdProfiles.values()) {
-			if(headerValue.equals(ldType.getHeaderValue())) {
-				return ldType;
-			}
-		}
-		throw new UserSetProfileValidationException(headerValue);		
-	}
-	
-	/**
-	 * 
-	 * @param name
-	 * @return
-	 * @throws UserSetProfileValidationException
-	 */
-	public static LdProfiles getByName(String name) throws UserSetProfileValidationException{
-		
-		for(LdProfiles ldType : LdProfiles.values()){
-			if(name.equalsIgnoreCase(ldType.name())) {
-				return ldType;
-			}
-		}
-		throw new UserSetProfileValidationException(name);		
-	}
-	
-	@Override
-	public String getHeaderValue() {
-		return headerValue;
-	}
-	
-	@Override
-	public String toString() {
-		return getHeaderValue();
-	}
+    private String headerValue;
+    private String preferHeaderValue;
 
-	public String getPreferHeaderValue() {
-		return preferHeaderValue;
+    LdProfiles(String headerValue, String preferHeaderValue) {
+	this.headerValue = headerValue;
+	this.preferHeaderValue = preferHeaderValue;
+    }
+
+    /**
+     * Identifying requested profile by Linked Data value. For user friendliness the
+     * the comparison is case insensitive
+     * 
+     * @param headerValue
+     * @return
+     * @throws UserSetProfileValidationException
+     */
+    public static LdProfiles getByHeaderValue(String headerValue) throws UserSetProfileValidationException {
+
+	for (LdProfiles ldType : LdProfiles.values()) {
+	    if (headerValue.equals(ldType.getHeaderValue())) {
+		return ldType;
+	    }
 	}
-	
+	throw new UserSetProfileValidationException(headerValue);
+    }
+
+    /**
+     * 
+     * @param name
+     * @return
+     * @throws UserSetProfileValidationException
+     */
+    public static LdProfiles getByName(String name) throws UserSetProfileValidationException {
+
+	for (LdProfiles ldType : LdProfiles.values()) {
+	    if (name.equalsIgnoreCase(ldType.name())) {
+		return ldType;
+	    }
+	}
+	throw new UserSetProfileValidationException(name);
+    }
+
+    @Override
+    public String getHeaderValue() {
+	return headerValue;
+    }
+
+    @Override
+    public String toString() {
+	return getHeaderValue();
+    }
+
+    public String getPreferHeaderValue() {
+	return preferHeaderValue;
+    }
+
 }
---
+++ set-definitions/src/main/java/eu/europeana/set/definitions/model/vocabulary/ProfileConstants.java
@@ -0,0 +1,14 @@
+package eu.europeana.set.definitions.model.vocabulary;
+
+public abstract class ProfileConstants {
+
+    public static final String VALUE_LD_CONTAINEDIRIS = "http://www.w3.org/ns/oa#PreferContainedIRIs";
+    public static final String VALUE_LD_MINIMAL = "http://www.w3.org/ns/ldp#PreferMinimalContainer";
+    public static final String VALUE_LD_ITEM_DESCRIPTIONS = "http://www.w3.org/ns/oa#PreferContainedDescription";
+    public static final String COMMON_STRING = "return=representation;include=\"";
+
+    public static final String VALUE_PREFER_CONTAINEDIRIS = COMMON_STRING + VALUE_LD_CONTAINEDIRIS + "\"";
+    public static final String VALUE_PREFER_MINIMAL = COMMON_STRING + VALUE_LD_MINIMAL + "\"";
+    public static final String VALUE_PREFER_ITEM_DESCRIPTIONS = COMMON_STRING + VALUE_LD_ITEM_DESCRIPTIONS + "\"";
+
+}
