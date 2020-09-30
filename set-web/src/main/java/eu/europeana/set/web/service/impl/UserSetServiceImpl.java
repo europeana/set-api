da4a0dcd019ceddcfc1d6b4b92911a2554376e23
srishti <srishti.singh@europeana.eu>
2020-09-30 17:27:18 +0200

    EA-2190 fix null pointer

--- set-web/src/main/java/eu/europeana/set/web/service/impl/UserSetServiceImpl.java
+++ set-web/src/main/java/eu/europeana/set/web/service/impl/UserSetServiceImpl.java
@@ -467,11 +467,7 @@ public class UserSetServiceImpl extends BaseUserSetServiceImpl implements UserSe
     public UserSet fetchItems(UserSet userSet, String sort, String sortOrder, int pageNr, int pageSize,
 	    LdProfiles profile) throws HttpException {
 
-    	System.out.println("USERSETS "+userSet.isOpenSet());
-		System.out.println("USERSETS "+userSet.getItems());
-		System.out.println("USERSETS "+userSet.getItems().isEmpty());
-
-		if (!userSet.isOpenSet() && (userSet.getItems() == null) || userSet.getItems().isEmpty()) {
+		if (!userSet.isOpenSet() && (userSet.getItems() == null || userSet.getItems().isEmpty())) {
 	    // if empty closed userset, nothing to do
 	    return userSet;
 	}
