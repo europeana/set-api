32b0775c68dcadbb5741f62720aa176a25440fd0
GordeaS <GordeaS@N3DSS1923.D03.arc.local>
2020-10-01 19:22:53 +0200

    #removed redundant context files and beans

--- set-web/src/test/java/eu/europeana/set/web/service/impl/WebUserSetServiceTest.java
+++ set-web/src/test/java/eu/europeana/set/web/service/impl/WebUserSetServiceTest.java
@@ -31,8 +31,7 @@ import eu.europeana.set.web.service.UserSetService;
  * Unit test for the Web UserSet service
  */
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(locations = { "classpath:set-web-test.xml", "classpath:set-web-context.xml",
-	"classpath:set-web-beans.xml", "set-definitions-context.xml",  "set-mongo-context.xml"})
+@ContextConfiguration(locations = { "classpath:set-web-test.xml"})
 public class WebUserSetServiceTest {
 
     private String TEST_DATASET_ID = "000000";
