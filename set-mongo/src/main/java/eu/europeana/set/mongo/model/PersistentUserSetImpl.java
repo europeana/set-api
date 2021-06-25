f668c476c6ce04f668e6e86cb7b7a4a6e3c940ca
GordeaS <GordeaS@N3DSS1923.D03.arc.local>
2021-06-25 17:29:23 +0200

    #EA-2599 configure text index

--- set-mongo/src/main/java/eu/europeana/set/mongo/model/PersistentUserSetImpl.java
+++ set-mongo/src/main/java/eu/europeana/set/mongo/model/PersistentUserSetImpl.java
@@ -1,7 +1,13 @@
 package eu.europeana.set.mongo.model;
 
 import org.bson.types.ObjectId;
-import org.mongodb.morphia.annotations.*;
+import org.mongodb.morphia.annotations.Entity;
+import org.mongodb.morphia.annotations.Field;
+import org.mongodb.morphia.annotations.Id;
+import org.mongodb.morphia.annotations.Index;
+import org.mongodb.morphia.annotations.IndexOptions;
+import org.mongodb.morphia.annotations.Indexes;
+import org.mongodb.morphia.utils.IndexType;
 
 import eu.europeana.set.definitions.model.impl.BaseUserSet;
 import eu.europeana.set.definitions.model.vocabulary.WebUserSetFields;
@@ -16,7 +22,10 @@ import eu.europeana.set.mongo.model.internal.PersistentUserSet;
     @Index(fields = {@Field(WebUserSetFields.CONTRIBUTOR)}),
     @Index(fields = {@Field(WebUserSetFields.SUBJECT)}),
     @Index(fields = {@Field(WebUserSetFields.ITEMS)}),
-    @Index(fields = {@Field(WebUserSetModelFields.MODIFIED)})})
+    @Index(fields = {@Field(WebUserSetModelFields.MODIFIED)}),
+    @Index(options = @IndexOptions(name = "text"), fields = {@Field(value = WebUserSetFields.SUBJECT, type = IndexType.TEXT), @Field(value = WebUserSetFields.DESCRIPTION, type = IndexType.TEXT)})
+
+})
 public class PersistentUserSetImpl extends BaseUserSet implements PersistentUserSet {
 
 	@Id
