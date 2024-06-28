3ad7371b0264e4605e2d4e99cbce439ecec9aa71
GordeaS <GordeaS@N3DSS2334.D03.arc.local>
2024-06-28 14:02:09 +0200

    type must not be uppercase

--- set-mongo/src/main/java/eu/europeana/set/web/model/WebUser.java
+++ set-mongo/src/main/java/eu/europeana/set/web/model/WebUser.java
@@ -3,6 +3,7 @@ package eu.europeana.set.web.model;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import eu.europeana.set.definitions.model.agent.impl.Person;
+import eu.europeana.set.definitions.model.vocabulary.AgentTypes;
 import eu.europeana.set.definitions.model.vocabulary.WebUserSetModelFields;
 
 @JsonInclude(value = JsonInclude.Include.NON_EMPTY)
@@ -12,10 +13,9 @@ public class WebUser extends Person {
     super();
   }
 
-  @Override
   @JsonProperty(WebUserSetModelFields.TYPE)
-  public String getType() {
-    return super.getType();
+  public String getWebType() {
+    return AgentTypes.PERSON.getJsonValue();
   }
 
   @Override
