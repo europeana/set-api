4fa53c0ffb119479077bd7f476ec0b05e6cbfd13
srishti <srishti.singh@europeana.eu>
2024-12-17 10:16:47 +0100

    EA-4015 remove disabled

--- set-integration-testing/src/integration-test/java/eu/europeana/api/set/integration/client/ClientConfigurationTest.java
+++ set-integration-testing/src/integration-test/java/eu/europeana/api/set/integration/client/ClientConfigurationTest.java
@@ -2,11 +2,9 @@ package eu.europeana.api.set.integration.client;
 
 import eu.europeana.set.client.config.ClientConfiguration;
 import org.apache.commons.lang3.StringUtils;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@Disabled("needs configuration file")
 public class ClientConfigurationTest {
 
     @Test
