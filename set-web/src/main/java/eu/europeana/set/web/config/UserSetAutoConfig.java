becc36464615dea60dcaee16d9068a867c7cf7e1
Shwetambara Nazare <nshweta90@gmail.com>
2025-11-25 16:41:14 +0100

    Update constructor arguments

--- set-web/src/main/java/eu/europeana/set/web/config/UserSetAutoConfig.java
+++ set-web/src/main/java/eu/europeana/set/web/config/UserSetAutoConfig.java
@@ -6,7 +6,6 @@ import eu.europeana.api.commons_sb3.error.i18n.I18nService;
 import eu.europeana.api.commons_sb3.error.i18n.I18nServiceImpl;
 import eu.europeana.api.commons_sb3.oauth2.service.impl.EuropeanaClientDetailsService;
 import java.nio.charset.StandardCharsets;
-import java.util.Properties;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
@@ -36,16 +35,9 @@ public class UserSetAutoConfig{
   public EuropeanaClientDetailsService getApiKeyClientDetailsService(){
     EuropeanaClientDetailsService clientDetails = new EuropeanaClientDetailsService();
     clientDetails.setApiKeyServiceUrl(apikeyServiceUrl);
-    AuthenticationConfig config = new AuthenticationConfig(loadProperties());
+    AuthenticationConfig config = new AuthenticationConfig(tokenEndpoint,grantParams);
     clientDetails.setAuthHandler(AuthenticationBuilder.newAuthentication(config));
     return clientDetails;
-  }
-
-  private Properties loadProperties() {
-    Properties properties = new Properties();
-    properties.setProperty(AuthenticationConfig.CONFIG_TOKEN_ENDPOINT,tokenEndpoint);
-    properties.setProperty(AuthenticationConfig.CONFIG_GRANT_PARAMS,grantParams);
-    return properties;
   }
 
   @Bean(name = BeanNames.BEAN_I18N_MESAGE_SOURCE)
